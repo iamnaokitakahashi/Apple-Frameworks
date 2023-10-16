4d05c733795bc1f1a2587822ec91e5947dfe6cf7
Naoki Takahashi <naokitakabusiness@gmail.com>
2023-10-16 12:56:30 +0900

    made button file

--- Apple-Frameworks/FrameworkDetailView.swift
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -11,13 +11,31 @@ struct FrameworkDetailView: View {
     var framework: Framework
     var body: some View {
         VStack {
+            Spacer()
             FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Text("Learn More")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .frame(width: 200, height: 50)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
     }
 }
 
 struct FrameworkDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        FrameworkDetailView(framework: MockData.sampleFramework) 
+        FrameworkDetailView(framework: MockData.sampleFramework)
     }
 }
