56fe45c28f22a225be5e0694daa0ae10c19910b5
Naoki Takahashi <naokitakabusiness@gmail.com>
2023-10-19 14:53:50 +0900

    refactored !

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -11,14 +11,10 @@ struct FrameworkGridView: View {
     
     @StateObject var viewModel = FrameworkGridViewModel()
     
-    let columns: [GridItem] = [GridItem(.flexible()),
-                               GridItem(.flexible()),
-                               GridItem(.flexible())] // .fleixble - based on how many columns I have, itll fill the screen
-    
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: columns) {
+                LazyVGrid(columns: viewModel.columns) {
                     ForEach(MockData.frameworks) { framework in
                         FrameworkTitleView(framework: framework)
                             .onTapGesture {
@@ -37,26 +33,6 @@ struct FrameworkGridView: View {
         static var previews: some View {
             FrameworkGridView()
                 .preferredColorScheme(.dark)
-        }
-    }
-    
-    struct FrameworkTitleView: View {
-        
-        let framework: Framework
-        
-        var body: some View {
-            
-            VStack {
-                Image(framework.imageName)
-                    .resizable()
-                    .frame(width: 90, height: 90)
-                Text(framework.name)
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .scaledToFit() // ability to shrink
-                    .minimumScaleFactor(0.6) // shrink to minium
-            }
-            .padding()
         }
     }
     
--- Apple-Frameworks/FrameworkGridViewModel.swift
+++ Apple-Frameworks/FrameworkGridViewModel.swift
@@ -10,10 +10,12 @@ import SwiftUI
 final class FrameworkGridViewModel: ObservableObject {
     
     var selectedFramework: Framework? {
-        didSet {
-            isShowingDetailView = true
-        }
+        didSet { isShowingDetailView = true }
     }
     
     @Published var isShowingDetailView: Bool = false
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())] // .fleixble - based on how many columns I have, itll fill the screen
 }
